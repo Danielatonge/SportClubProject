bbe52c968c82ba0bd1a87698e6faa9cb80f7b47b
Djotchuang <djotchuangtamo@gmail.com>
2021-09-12 11:16:21 +0100

    Setup & started dev server

--- app.py
+++ app.py
@@ -7,14 +7,18 @@ app.config['DEBUG'] = True
 Scss(app, static_dir='static/css', asset_dir='static/scss')
 
 
+@app.route('/')
+def main_page2():
+    return render_template('Главная/page2.html', title="Главная") 
+
 @app.route('/Главная/page1')
 def main_page1():
     return render_template('Главная/page1.html', title="Главная")
 
 
-@app.route('/Главная/page2')
-def main_page2():
-    return render_template('Главная/page2.html', title="Главная")
+# @app.route('/Главная/page2')
+# def main_page2():
+#     return render_template('Главная/page2.html', title="Главная")
 
 
 if __name__ == '__main__':
