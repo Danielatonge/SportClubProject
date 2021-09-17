ab29bedea633873f058c74c51773f877c27abcdf
elamboatonge <elamboatonge@gmail.com>
2021-09-17 06:41:05 +0300

    npm in python project error

--- app.py
+++ app.py
@@ -8,9 +8,9 @@ Scss(app, static_dir='static/css', asset_dir='static/scss')
 
 
 @app.route('/')
-
 def main():
     return redirect(url_for('main_page1'))
+
 
 @app.route('/Главная/page1')
 def main_page1():
@@ -22,17 +22,6 @@ def main_page2():
     return render_template('Главная/page2.html', title="Главная")
 
 
-# ------------------------- Игры
-@app.route('/игры/page1')
-def play_page1():
-    return render_template('игры/page 1.html', title="Игры")
-
-
-@app.route('/игры/page2')
-def play_page2():
-    return render_template('игры/page 2.html', title="Игры")
-
-
 # ------------------------- Киберзона
 @app.route('/киберзона/page1')
 def kiberzone_page1():
@@ -53,6 +42,7 @@ def kiberzone_page3():
 def kiberzone_page4():
     return render_template('киберзона/page 4.html', title="Киберзона")
 
+
 # ------------------------- Игры
 @app.route('/игры/page1')
 def play_page1():
@@ -62,27 +52,6 @@ def play_page1():
 @app.route('/игры/page2')
 def play_page2():
     return render_template('игры/page 2.html', title="Игры")
-
-
-# ------------------------- Киберзона
-@app.route('/киберзона/page1')
-def kiberzone_page1():
-    return render_template('киберзона/page 1.html', title="Киберзона")
-
-
-@app.route('/киберзона/page2')
-def kiberzone_page2():
-    return render_template('киберзона/page 2.html', title="Киберзона")
-
-
-@app.route('/киберзона/page3')
-def kiberzone_page3():
-    return render_template('киберзона/page 3.html', title="Киберзона")
-
-
-@app.route('/киберзона/page4')
-def kiberzone_page4():
-    return render_template('киберзона/page 4.html', title="Киберзона")
 
 
 # ------------------------- Клубы
