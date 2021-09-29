5f5ccca508e47a3aca0f48848d1c601e41e51b67
Djotchuang <djotchuangtamo@gmail.com>
2021-09-29 10:20:41 +0100

    All sliders, responsiveness, markup, & styling for Клубы pages implemented

--- static/js/script.js
+++ static/js/script.js
@@ -11,46 +11,37 @@ $(document).ready(function () {
   /*-------------------------------------
     OwlCarousel
     -------------------------------------*/
-  $(".rs-carousel").each(function () {
-    var owlCarousel = $(this),
+  $(".rs-team .rs-carousel").each(function () {
+    const owlCarousel = $(this),
       loop = owlCarousel.data("loop"),
       items = owlCarousel.data("items"),
       margin = owlCarousel.data("margin"),
-      stagePadding = owlCarousel.data("stage-padding"),
       autoplay = owlCarousel.data("autoplay"),
       autoplayTimeout = owlCarousel.data("autoplay-timeout"),
       smartSpeed = owlCarousel.data("smart-speed"),
-      dots = owlCarousel.data("dots"),
       nav = owlCarousel.data("nav"),
       navSpeed = owlCarousel.data("nav-speed"),
       xsDevice = owlCarousel.data("mobile-device"),
       xsDeviceNav = owlCarousel.data("mobile-device-nav"),
-      xsDeviceDots = owlCarousel.data("mobile-device-dots"),
       smDevice = owlCarousel.data("ipad-device"),
       smDeviceNav = owlCarousel.data("ipad-device-nav"),
-      smDeviceDots = owlCarousel.data("ipad-device-dots"),
       smDevice2 = owlCarousel.data("ipad-device2"),
       smDeviceNav2 = owlCarousel.data("ipad-device-nav2"),
-      smDeviceDots2 = owlCarousel.data("ipad-device-dots2"),
-      mdDevice = owlCarousel.data("md-device"),
-      centerMode = owlCarousel.data("center-mode"),
-      mdDeviceNav = owlCarousel.data("md-device-nav"),
-      mdDeviceDots = owlCarousel.data("md-device-dots");
+      centerMode = owlCarousel.data("center-mode");
+
     owlCarousel.owlCarousel({
       loop: loop ? true : false,
       items: items ? items : 3,
       lazyLoad: true,
       center: centerMode ? true : false,
       margin: margin ? margin : 0,
-      //stagePadding: (stagePadding ? stagePadding : 0),
       autoplay: autoplay ? true : false,
       autoplayTimeout: autoplayTimeout ? autoplayTimeout : 1000,
       smartSpeed: smartSpeed ? smartSpeed : 250,
-      dots: dots ? true : false,
       nav: nav ? true : false,
       navText: [
-        "<i class='fa fa-angle-left'></i>",
-        "<i class='fa fa-angle-right'></i>",
+        "<i class='fa fa-arrow-left'></i>",
+        "<i class='fa fa-arrow-right'></i>",
       ],
       navSpeed: navSpeed ? true : false,
       responsiveClass: true,
@@ -58,25 +49,122 @@ $(document).ready(function () {
         0: {
           items: xsDevice ? xsDevice : 1,
           nav: xsDeviceNav ? true : false,
-          dots: xsDeviceDots ? true : false,
           center: false,
         },
         576: {
-          items: smDevice2 ? smDevice2 : 2,
-          nav: smDeviceNav2 ? true : false,
-          dots: smDeviceDots2 ? true : false,
+          items: smDevice ? smDevice : 2,
+          nav: smDeviceNav ? true : false,
           center: false,
         },
         768: {
-          items: smDevice ? smDevice : 3,
+          items: smDevice2 ? smDevice2 : 3,
+          nav: smDeviceNav2 ? true : false,
+          center: false,
+        },
+      },
+    });
+  });
+
+  $(".rs-video .rs-carousel").each(function () {
+    const owlCarousel = $(this),
+      loop = owlCarousel.data("loop"),
+      items = owlCarousel.data("items"),
+      margin = owlCarousel.data("margin"),
+      autoplay = owlCarousel.data("autoplay"),
+      autoplayTimeout = owlCarousel.data("autoplay-timeout"),
+      smartSpeed = owlCarousel.data("smart-speed"),
+      nav = owlCarousel.data("nav"),
+      navSpeed = owlCarousel.data("nav-speed"),
+      xsDevice = owlCarousel.data("mobile-device"),
+      xsDeviceNav = owlCarousel.data("mobile-device-nav"),
+      smDevice = owlCarousel.data("ipad-device"),
+      smDeviceNav = owlCarousel.data("ipad-device-nav"),
+      centerMode = owlCarousel.data("center-mode");
+    owlCarousel.owlCarousel({
+      loop: loop ? true : false,
+      items: items ? items : 2,
+      lazyLoad: true,
+      center: centerMode ? true : false,
+      margin: margin ? margin : 0,
+      autoplay: autoplay ? true : false,
+      autoplayTimeout: autoplayTimeout ? autoplayTimeout : 1000,
+      smartSpeed: smartSpeed ? smartSpeed : 250,
+      nav: nav ? true : false,
+      navText: [
+        "<i class='fa fa-arrow-left'></i>",
+        "<i class='fa fa-arrow-right'></i>",
+      ],
+      navSpeed: navSpeed ? true : false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: xsDevice ? xsDevice : 1,
+          nav: xsDeviceNav ? true : false,
+          center: false,
+        },
+        576: {
+          items: smDevice ? smDevice : 2,
           nav: smDeviceNav ? true : false,
-          dots: smDeviceDots ? true : false,
+          center: false,
+        },
+      },
+    });
+  });
+
+  $(".rs-image .rs-carousel").each(function () {
+    const owlCarousel = $(this),
+      loop = owlCarousel.data("loop"),
+      items = owlCarousel.data("items"),
+      margin = owlCarousel.data("margin"),
+      autoplay = owlCarousel.data("autoplay"),
+      autoplayTimeout = owlCarousel.data("autoplay-timeout"),
+      smartSpeed = owlCarousel.data("smart-speed"),
+      nav = owlCarousel.data("nav"),
+      navSpeed = owlCarousel.data("nav-speed"),
+      xsDevice = owlCarousel.data("mobile-device"),
+      xsDeviceNav = owlCarousel.data("mobile-device-nav"),
+      smDevice = owlCarousel.data("ipad-device"),
+      smDeviceNav = owlCarousel.data("ipad-device-nav"),
+      smDevice2 = owlCarousel.data("ipad-device2"),
+      smDeviceNav2 = owlCarousel.data("ipad-device-nav2"),
+      centerMode = owlCarousel.data("center-mode"),
+      mdDevice = owlCarousel.data("md-device"),
+      mdDeviceNav = owlCarousel.data("md-device-nav");
+    owlCarousel.owlCarousel({
+      loop: loop ? true : false,
+      items: items ? items : 4,
+      lazyLoad: true,
+      center: centerMode ? true : false,
+      margin: margin ? margin : 0,
+      autoplay: autoplay ? true : false,
+      autoplayTimeout: autoplayTimeout ? autoplayTimeout : 1000,
+      smartSpeed: smartSpeed ? smartSpeed : 250,
+      nav: nav ? true : false,
+      navText: [
+        "<i class='fa fa-arrow-left'></i>",
+        "<i class='fa fa-arrow-right'></i>",
+      ],
+      navSpeed: navSpeed ? true : false,
+      responsiveClass: true,
+      responsive: {
+        0: {
+          items: xsDevice ? xsDevice : 1,
+          nav: xsDeviceNav ? true : false,
+          center: false,
+        },
+        576: {
+          items: smDevice ? smDevice : 2,
+          nav: smDeviceNav ? true : false,
+          center: false,
+        },
+        768: {
+          items: smDevice2 ? smDevice2 : 3,
+          nav: smDeviceNav2 ? true : false,
           center: false,
         },
         992: {
           items: mdDevice ? mdDevice : 4,
           nav: mdDeviceNav ? true : false,
-          dots: mdDeviceDots ? true : false,
         },
       },
     });
